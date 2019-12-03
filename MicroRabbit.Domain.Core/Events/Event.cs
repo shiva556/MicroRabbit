18a232783abb73838d316549da6c4c5c67ca1b67
Sivaji Koppula <SKoppula@logisticsdev.intra>
2019-12-03 16:59:35 +0530

    Add project files.

---
+++ MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event : IRequest<bool>
+    {
+        public String EventType { get; protected set; }
+
+        protected Event()
+        {
+            EventType = this.GetType().Name;
+        }
+    }
+}
---
+++ MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Message()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
